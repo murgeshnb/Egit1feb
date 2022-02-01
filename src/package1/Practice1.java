33c55e300540ba240ce636c9f120a17fc2c10929
murgeshnb <mnbechetti@gmail.com>
2022-02-01 11:33:34 +0530

    User1 first comment

---
+++ src/package1/Practice1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("USer 1 first push");
+
+	}
+
+}
